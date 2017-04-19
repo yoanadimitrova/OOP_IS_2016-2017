1121896309dea235c7447da22572ae75993fe478
yoanadimitrova <ioana.6a@mail.bg>
2017-04-19 10:05:37 +0300

    Update BigInt.h

--- OOP_prkatikum_HW_2/BigInt.h
+++ OOP_prkatikum_HW_2/BigInt.h
@@ -7,8 +7,8 @@ using namespace std;
 class BigInt
 {
 private:
-    int number; //Създаване на обект от цяло число
-    char* bigNumber;//Създаване на обект от от низ, съдържащ само десетични цифри(възможно е да съдържа минус на първата си позиция)
+    int number; 
+    char* bigNumber;
 
     void copy(const BigInt&);
     void destroy();
@@ -33,17 +33,17 @@ public:
     void concat(char* other);
 
     //Redefinition of operators:
-    BigInt& operator=(const BigInt& rhs); //Оперататор за присвояване, който приема като десен аргумент обект от тип BigInt
-    BigInt& operator=(int rhs); //Оперататор за присвояване, който приема като десен аргумент обект цяло число
-    const BigInt operator+(const BigInt& rhs); // Оператор +
-    BigInt& operator+=(const BigInt& rhs); // Оператор +=
-    BigInt& operator++(); // Префиксен оператор ++
-    const BigInt& operator++(int);// Постфиксен оператор ++
-    const BigInt& operator-(const BigInt& rhs); // Оператор -
-    BigInt& operator-=(const BigInt& rhs); // Оператор -=
-    BigInt& operator--(); // Префиксен оператор --
-    const BigInt& operator--(int); // Постфиксен оператор --
+    BigInt& operator=(const BigInt& rhs); 
+    BigInt& operator=(int rhs);
+    const BigInt operator+(const BigInt& rhs);
+    BigInt& operator+=(const BigInt& rhs);
+    BigInt& operator++();
+    const BigInt& operator++(int);
+    const BigInt& operator-(const BigInt& rhs); 
+    BigInt& operator-=(const BigInt& rhs); 
+    BigInt& operator--();
+    const BigInt& operator--(int); 
 
-    friend std::ostream& operator<<(std::ostream& out, const BigInt& rhs); // Оператор за изход <<
-    friend std::istream& operator>>(std::istream& in, BigInt& rhs); // Оператор за вход >>
+    friend std::ostream& operator<<(std::ostream& out, const BigInt& rhs); 
+    friend std::istream& operator>>(std::istream& in, BigInt& rhs); 
 };
