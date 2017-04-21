20df1d3c536a4f19bfb3beaa29040e7be8c1836c
yoanadimitrova <ioana.6a@mail.bg>
2017-04-21 21:05:01 +0300

    Add files via upload

---
+++ Templates.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+//Templates:
+
+template <class T>
+T getMax(T a, T b)
+{
+    return a > b ? a : b;
+}
+
+void swap (int& a, int& b);
+template <typename T> //nqma znachenie dali e class ili typename
+void swap_t(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    cout << getMax <int> (5,6);
+    cout << getMax<double> (5.4, 6.0);
+    int a = 3;
+    int b = 5;
+    swap_t<int>(a, b);
+//    cout << swap_t;
+    return 0;
+}
